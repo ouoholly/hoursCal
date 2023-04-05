8a80450a551e2623f9abe7537c3d4be67eb12293
ouoholly <60885102+ouoholly@users.noreply.github.com>
2023-04-05 13:05:43 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
 $('input').keyup(function(){
 
+	// input
     var start_h  = Number($('#start').val().slice(0, 2));  
 	var start_m  = Number($('#start').val().slice(2, 4));  
-
 	var end_h  = Number($('#end').val().slice(0, 2));  
 	var end_m  = Number($('#end').val().slice(2, 4)); 
-	
     var lunchValue  = Number($('#lunch').val());
 
+	// calculate
 	var diff_h = ((end_h*60 + end_m) - (start_h*60 + start_m) - lunchValue) / 60
 	var diff_m = ((end_h*60 + end_m) - (start_h*60 + start_m) - lunchValue) % 60
 
-	// calculate
+	// display result
     $('#total_h').html(Math.floor(diff_h) + " hours"); 
 	$('#total_m').html(diff_m + " minutes"); 
 	$('#total_decimal').html(diff_h.toFixed(5) + " hours"); 
@@ -19,18 +19,20 @@ $('input').keyup(function(){
 	$('#start_decimal').html(((start_h*60 + start_m)/60).toFixed(2)); 
 	$('#end_decimal').html(((end_h*60 + end_m)/60).toFixed(2)); 
 
-	// to 12-hour format
-	if (start_h > 11) {
-		$('#start_12f').html(start_h - 12 + ":" + String(start_m).padStart(2, '0') + " PM"); 
-	}
-	else {
+	// display the input in 12-hour format
+	if (start_h > 12) {
+		$('#start_12f').html(start_h - 12 + ":" + String(start_m).padStart(2, '0') + " PM");
+	} else if (start_h === 12) {
+		$('#start_12f').html(start_h + ":" + String(start_m).padStart(2, '0') + " PM");
+	} else {
 		$('#start_12f').html(start_h + ":" + String(start_m).padStart(2, '0') + " AM"); 
 	}
 
-	if (end_h > 11) {
+	if (end_h > 12) {
 		$('#end_12f').html(end_h - 12 + ":" + String(end_m).padStart(2, '0') + " PM"); 
-	}
-	else {
+	} else if (end_h === 12) {
+		$('#end_12f').html(end_h + ":" + String(end_m).padStart(2, '0') + " PM");
+	} else {
 		$('#end_12f').html(end_h + ":" + String(end_m).padStart(2, '0') + " AM"); 
 	}
 
